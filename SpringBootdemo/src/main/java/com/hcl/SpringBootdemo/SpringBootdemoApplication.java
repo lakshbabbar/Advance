87e66f3d89ed0f5146d5a155fceae0555577f663
Laksh Babbar <43206740+lakshbabbar@users.noreply.github.com>
2019-09-24 18:40:22 +0530

    Add files via upload

---
+++ SpringBootdemo/src/main/java/com/hcl/SpringBootdemo/SpringBootdemoApplication.java
@@ -0,0 +1,26 @@
+package com.hcl.SpringBootdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootdemoApplication {
+
+	@Value("${spring.application.name}")
+	private String name;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootdemoApplication.class, args);
+	}
+	
+	
+	@RequestMapping(value="/")
+	public String getName(){
+		return name;
+	}
+
+}
